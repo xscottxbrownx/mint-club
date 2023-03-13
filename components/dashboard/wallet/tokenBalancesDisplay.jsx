d4dd17cbff3124cff82172bdc49a7d5eb3bacb59
veganedge <scottxbrown@yahoo.com>
2023-03-13 16:31:53 -0700

    Putting inputs at top of TokenBalancesDisplay.jsx

--- components/dashboard/wallet/tokenBalancesDisplay.jsx
+++ components/dashboard/wallet/tokenBalancesDisplay.jsx
@@ -6,25 +6,54 @@ import { useAccount } from "wagmi";
 import styles from "../../../styles/dashboard/TokenBalancesDisplay.module.css";
 
 
-export default function TokenBalancesDisplay({ walletAddress, chain }) {
+export default function TokenBalancesDisplay() {
   const [tokensBalance, setTokensBalance] = useState();
+  const [walletOrAddress, setWalletOrAddress] = useState("vitalik.eth");
   const [isLoading, setIsloading] = useState(false);
-  const { address, isConnected, isDisconnected } = useAccount();
+  const [fetchMethod, setFetchMethod] = useState("wallet");
+  const [chain, setChain] = useState(process.env.NEXT_PUBLIC_ALCHEMY_NETWORK);
+  const { address, isConnected } = useAccount();
+
+
+// set fetchMethod based on selection (wallet address, collection, or connected wallet)
+const changeFetchMethods = (e) => {
+  // setNfts();
+  // setPageKey();
+  switch (e.target.value) {
+    case "wallet":
+      setWalletOrAddress("vitalik.eth");
+      break;
+    case "connectedWallet":
+      setWalletOrAddress(address);
+      break;
+  }
+  setFetchMethod(e.target.value);
+};
+
+  
+
+  
+  
+
+
+
+
+
+// =====================================================
 
   // ON COMPONENT MOUNT + isConnected STATE CHANGE:
-  // if there was a wallet address passed to the component
-  // or if a wallet is connected to the site
+  // if a wallet is connected to the site
   // try to fetch the token balances in wallet
   useEffect(() => {
     const getWalletBalance = async () => {
       setIsloading(true);
-      if (isConnected || walletAddress) {
+      if (address) {
         try {
           const fetchedTokensBalance = await fetch("/api/getTokensBalance", {
             method: "POST",
             body: JSON.stringify({
-              address: walletAddress ? walletAddress : address,
-              chain,
+              address: address,
+              chain: process.env.NEXT_PUBLIC_ALCHEMY_NETWORK,
             }),
           }).then((res) => res.json());
           setTokensBalance(fetchedTokensBalance);
@@ -37,61 +66,113 @@ export default function TokenBalancesDisplay({ walletAddress, chain }) {
     getWalletBalance();
   }, [isConnected]);
 
-  // if no wallet is connected
-  // display instructions to connect wallet
-  if (isDisconnected || !walletAddress) {
-    return (
-      <div className={styles.token_panel_container}>
-        <div className={styles.token_box}>
-          <h2>CONNECT WALLET TO SEE TOKEN BALANCES</h2>
-        </div>
-      </div>
-    );
-  }
+  // get wallet balance if fetchMethod changes
+  // useEffect(() => {
+  //   getWalletBalance();
+  // }, [fetchMethod]);
   
-  // if wallet is connected but currently fetching data
-  // display Loading...
-  if (isLoading) {
-    return <p>Loading...</p>;
-  }
 
-  // when wallet is connected
-  // display wallet address and token balances
+
   return (
-    <div className={styles.token_panel_container}>
-      <div className={styles.token_box}>
+    <div className={styles.wallet_page}>
+      <div>
 
-        <h2>{`${address.slice(0, 6)}...${address.slice(38)}`}</h2>
+        {/* ===== START OF RENDER TOP INPUTS ===== */}
+        <div className={styles.fetch_selector_container}>
+          <h2 style={{ fontSize: "20px" }}>Explore tokens by</h2>
 
-        {/* display each token and it's balance from the wallet address */}
-        <div className={styles.tokens_container}>
-          {tokensBalance?.length &&
-            tokensBalance.map((token) => {
-              return (
-                <div key={token.symbol} className={styles.token}>
-                  <div className={styles.token_name_container}>
-                    <div className={styles.image_container}>
-                      <img
-                        src={
-                          token.logo
-                            ? token.logo
-                            : "http://fpoimg.com/40x40"
-                        }
-                      />
-                    </div>
-                    <p className={styles.token_name}>{token.name}</p>
-                  </div>
-                  <div className={styles.info_container}>
-                    <div className={styles.token_name_symbol_container}>
-                      <p className={styles.token_balance}>{token.balance}</p>
-                      <p className={styles.token_symbol}>{token.symbol}</p>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+          {/* select the fetchMethod */}
+          <div className={styles.select_container}>
+            <select
+              defaultValue={"wallet"}
+              onChange={(e) => {changeFetchMethods(e)}}
+            >
+              <option value={"wallet"}>address</option>
+              <option value={"connectedWallet"}>connected wallet</option>
+            </select>
+          </div>
+        </div>
+
+        <div className={styles.inputs_container}>
+          <div className={styles.input_button_container}>
+            {/* wallet address input */}
+            <input
+              value={walletOrAddress}
+              onChange={(e) => {setWalletOrAddress(e.target.value)}}
+              placeholder="Insert wallet address"
+            />
+            <div className={styles.buttons_under_input}>
+              {/* select the blockchain */}
+              <div className={styles.select_container_alt}>
+                <select
+                  onChange={(e) => {setChain(e.target.value)}}
+                  defaultValue={process.env.ALCHEMY_NETWORK}
+                >
+                  <option value={"ETH_MAINNET"}>Mainnet</option>
+                  <option value={"MATIC_MAINNET"}>Polygon</option>
+                  <option value={"ETH_GOERLI"}>Goerli</option>
+                  <option value={"MATIC_MUMBAI"}>Mumbai</option>
+                </select>
+              </div>
+              {/* search button */}
+              <div onClick={() => fetchNFTs()} className={styles.button_blue}>
+                <a>Search</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      {/* ===== END OF RENDER TOP INPUTS ===== */}
+
+      {/* if loading data, RENDER Loading... */}
+      {isLoading ? (
+        <div className={styles.loading_box}>
+          <p>Loading...</p>
+        </div>
+      ) : (
+        address ? (
+        // if wallet is connected RENDER TOKEN BALANCE DIV
+        <div className={styles.token_panel_container}>
+          <div className={styles.token_box}>
+            <h2>{`${address.slice(0, 6)}...${address.slice(38)}`}</h2>
+            {/* display each token and it's balance from the wallet address */}
+            <div className={styles.tokens_container}>
+              {tokensBalance?.length && tokensBalance.map((token) => {
+                return (
+                  <div key={token.symbol} className={styles.token}>
+                    <div className={styles.token_name_container}>
+                      <div className={styles.image_container}>
+                        <img
+                          src={
+                            token.logo
+                              ? token.logo
+                              : "http://fpoimg.com/40x40"
+                          }
+                        />
+                      </div>
+                      <p className={styles.token_name}>{token.name}</p>
+                    </div>
+                    <div className={styles.info_container}>
+                      <div className={styles.token_symbol_container}>
+                        <p className={styles.token_balance}>{token.balance}</p>
+                        <p className={styles.token_symbol}>{token.symbol}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        ) : (
+          // if no wallet is NOT connected, RENDER INSTRUCTIONS to connect wallet
+          <div className={styles.token_panel_container}>
+            <div className={styles.token_box}>
+              <h2>CONNECT WALLET OR ENTER WALLET ADDRESS TO SEE TOKEN BALANCES</h2>
+            </div>
+          </div>
+          )
+      )}
     </div>
   );
 }
--- pages/Dashboard/Wallet.js
+++ pages/Dashboard/Wallet.js
@@ -1,16 +1,18 @@
 // Imported Components
 import DashboardLayout from "../../components/dashboard/DashboardLayout";
 import TokenBalancesDisplay from "../../components/dashboard/wallet/tokenBalancesDisplay.jsx"
-// Imports from Wagmi
-import { useAccount } from "wagmi";
+// Imported stylesheet
+import styles from "../../styles/dashboard/Wallet.module.css";
 
 
 export default function Wallet() {
-  const {address} = useAccount();
-  return(
-    <TokenBalancesDisplay walletAddress={address} chain={"ETH_MAINNET"}/>
+  return (
+    <div className={styles.main}>
+      <TokenBalancesDisplay />
+    </div>
   );
 }
+
 
 Wallet.getLayout = function getLayout(page) {
   return <DashboardLayout>{page}</DashboardLayout>;
