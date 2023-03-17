18be86a63319a0e44372f3cd32b9c00e495ff2e9
veganedge <scottxbrown@yahoo.com>
2023-03-17 07:53:16 -0700

    tbd2 - inefficient code, full comp. for each state

--- pages/Dashboard/Wallet.js
+++ pages/Dashboard/Wallet.js
@@ -1,5 +1,6 @@
 // Imported Components
 import DashboardLayout from "../../components/dashboard/DashboardLayout";
+import Tbd2 from "../../components/dashboard/wallet/tbd2";
 import TokenBalancesDisplay from "../../components/dashboard/wallet/tokenBalancesDisplay.jsx"
 // Imported stylesheet
 import styles from "../../styles/dashboard/Wallet.module.css";
@@ -8,7 +9,8 @@ import styles from "../../styles/dashboard/Wallet.module.css";
 export default function Wallet() {
   return (
     <div className={styles.main}>
-      <TokenBalancesDisplay />
+      <Tbd2 />
+      {/* <TokenBalancesDisplay /> */}
     </div>
   );
 }
