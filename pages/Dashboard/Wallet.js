c2b6d224d27cb688ce5d05450e3772d05661a554
veganedge <scottxbrown@yahoo.com>
2023-03-20 10:29:14 -0700

    deleting experimental render code

--- pages/Dashboard/Wallet.js
+++ pages/Dashboard/Wallet.js
@@ -1,6 +1,5 @@
 // Imported Components
 import DashboardLayout from "../../components/dashboard/DashboardLayout";
-import Tbd2 from "../../components/dashboard/wallet/tbd2";
 import TokenBalancesDisplay from "../../components/dashboard/wallet/tokenBalancesDisplay.jsx"
 // Imported stylesheet
 import styles from "../../styles/dashboard/Wallet.module.css";
@@ -9,8 +8,7 @@ import styles from "../../styles/dashboard/Wallet.module.css";
 export default function Wallet() {
   return (
     <div className={styles.main}>
-      <Tbd2 />
-      {/* <TokenBalancesDisplay /> */}
+      <TokenBalancesDisplay />
     </div>
   );
 }
